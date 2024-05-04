af3d68082c5c7c63e52a982ed095d31cab4af39b
Sammy <164060571+sbernstein99@users.noreply.github.com>
2024-05-03 21:53:31 -0700

    Update main.js

--- main.js
+++ main.js
@@ -9,12 +9,12 @@ const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
 const altText = {
-  'pic1.jpg': 'Aerial View of the Horseshoe Bend',
+  'pic1.jpg': 'An aerial view of the Horseshoe Bend',
   'pic2.jpg': 'A glowing waterfall amidst a grey sky',
   'pic3.jpg': 'A group of cacti amidst an Arizona sunset',
   'pic4.jpg': 'A close-up of the inside of a canyon',
   'pic5.jpg': 'A waterfall nestled within a rock formation amidst a sunny sky'
-};
+}
 
 /* Looping through images */
 for (const image of images) {
@@ -30,13 +30,14 @@ for (const image of images) {
 
 /* Wiring up the Darken/Lighten button */
 btn.addEventListener('click', () => {
-  if (btn.getAttribute('class') === 'dark') {
-    btn.setAttribute('class', 'light');
+  const btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark') {
+    btn.setAttribute('class','light');
     btn.textContent = 'Lighten';
-    overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
   } else {
-    btn.setAttribute('class', 'dark');
+    btn.setAttribute('class','dark');
     btn.textContent = 'Darken';
-    overlay.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
   }
 });
